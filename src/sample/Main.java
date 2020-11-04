07dfbe701681af495e8805cc32420eea99a683f6
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-11-04 15:52:30 +0530

    add 2 scenes- menu and game

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,42 +1,43 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application {
+    
+    Button start;
+    Button exit;
+    Scene menu,game;
 
-    Button buttonNewgame;
 
     @Override
     public void start(Stage primaryStage) throws Exception
 
     {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Label label1 = new Label("COLOR GAME");
+        Button start = new Button("Start");
+        start.setOnAction(e->primaryStage.setScene(game));
+
+        VBox layout1 = new VBox(50);
+        layout1.getChildren().addAll(label1,start);
+        menu = new Scene(layout1,300,500);
+
+        Button exit = new Button("Back to main menu");
+        exit.setOnAction(e->primaryStage.setScene(menu));
+
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(exit);
+        game = new Scene(layout2,300,500);
+
+        primaryStage.setScene(menu);
         primaryStage.setTitle("Color Switch");
-        buttonNewgame = new Button("Start");
-        buttonNewgame.setOnAction(this);
-
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(buttonNewgame);
-
-        primaryStage.setScene(new Scene(layout, 300, 275));
         primaryStage.show();
-    }
-
-
-    @Override
-    public void handle(ActionEvent event){
-        if(event.getSource()==buttonNewgame){
-            System.out.println("New game button pressed");
-        }
     }
 
     public static void main(String[] args) {
